975b713e2b1f6e4af3fcc1aa04b3b1bbb91c5802
kangseungmin <rkdtmdals999@naver.com>
2024-01-21 15:00:30 +0900

    [feat] 제네릭 클래스를 통해 서버에서 동일한 내용으로 Body 값 내려주기

--- src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
+++ src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.example.exception.exception;
 
+import com.example.exception.model.Api;
+import java.util.NoSuchElementException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,6 +28,22 @@ public class RestApiExceptionHandler {
         log.error("인덱스 에러만 잡는 메소드");
 
         return ResponseEntity.status(200).build();
+    }
+
+
+    // NoSuchElement 예외가 발생했을 때 Api 클래스 형식으로 바디 내려주기.
+    @ExceptionHandler(value = {NoSuchElementException.class})
+    public ResponseEntity<Api> noSuchElement(NoSuchElementException e) {
+        log.error("No Such Element 에러 발생", e);
+
+        var response = Api.builder()
+                .resultCode(String.valueOf(HttpStatus.NOT_FOUND.value()))
+                .resultMessage(HttpStatus.NOT_FOUND.getReasonPhrase())
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(response);
     }
 }
 
---
+++ src/main/java/com/example/exception/model/UserResponse.java
@@ -0,0 +1,18 @@
+package com.example.exception.model;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class) // snake case로 설정
+public class UserResponse {
+    private String id;
+    private String userName;
+    private Integer age;
+}
